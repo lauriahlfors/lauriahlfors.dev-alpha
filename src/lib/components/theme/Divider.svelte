14bb5b4971e002b8b111a465e835efaf28c868c8
lauriahlfors <lauriahlfors@proton.me>
2025-03-19 10:34:55 +0200

    feat: add base theme ui components

---
+++ src/lib/components/theme/Divider.svelte
@@ -0,0 +1,9 @@
+<div class="relative w-full">
+	<div
+		class="bg-background border-secondary absolute bottom-0 left-6 z-10 size-3 -translate-x-[5.5px] translate-y-[5.5px] rotate-45 border-[1px] lg:left-48 2xl:left-80"
+	></div>
+	<div class="bg-secondary h-[1px]"></div>
+	<div
+		class="bg-background border-secondary absolute right-6 bottom-0 z-10 size-3 translate-x-[5.5px] translate-y-[5.5px] rotate-45 border-[1px] lg:right-48 2xl:right-80"
+	></div>
+</div>
