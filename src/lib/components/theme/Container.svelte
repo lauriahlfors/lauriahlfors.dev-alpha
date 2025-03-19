74d01c0adc32f756d52441f4f0d1bffa6b3fde40
lauriahlfors <lauriahlfors@proton.me>
2025-03-19 10:34:55 +0200

    feat: add base theme ui components

---
+++ src/lib/components/theme/Container.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+	import { cn } from '$lib/utils';
+	import { type ClassValue } from 'clsx';
+
+	interface Props {
+		children?: Snippet;
+		className?: ClassValue;
+	}
+
+	let { children, className = '' }: Props = $props();
+</script>
+
+<div class={cn('border-secondary mx-6 border-x-[1px] p-4 lg:mx-48 2xl:mx-80 ' + className)}>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
