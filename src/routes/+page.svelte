2bd1b3e06c06ab290ce450074f01d5687f6570b2
lauriahlfors <lauriahlfors@proton.me>
2025-03-21 00:14:56 +0200

    feat: add experience section to index route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import Experience from '$lib/components/sections/Experience.svelte';
 	import Stack from '$lib/components/sections/Stack.svelte';
 </script>
 
+<Experience />
 <Stack />
